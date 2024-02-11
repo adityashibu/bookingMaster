839bb168affcf455db72baba5e75e25a4e19df9a
Aditya Shibu <as2397@hw.ac.uk>
2024-02-11 14:14:09 +0400

    Got mail automation working

--- testmail.py
+++ testmail.py
@@ -21,4 +21,4 @@ context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     smtp.login(email_sender, email_password)
-    smtp.sendmail(email_sender, email_reciever, em.as_string)
+    smtp.sendmail(email_sender, email_reciever, em.as_string())
